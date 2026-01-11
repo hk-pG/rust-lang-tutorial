06ab85335e91a82523c0258de906b2e3855bf290
hk-pG <hukada.gogotea@gmail.com>
2026-01-11 21:32:38 +0900

    del: delete fib library

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,9 @@
 fn main() {
-    let rect1 = Rectangle {
-        width: 30,
-        height: 50,
+    let some_u8_value = Some(0u8);
+
+    if let Some(3) = some_u8_value {
+        println!("three");
+    } else {
+        ()
     };
-    let rect2 = Rectangle {
-        width: 10,
-        height: 40,
-    };
-    let rect3 = Rectangle::square(30);
-
-    println!("rect1 is {:?}. area is {}", rect1, rect1.area());
-    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
-    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
-}
-
-#[derive(Debug)]
-struct Rectangle {
-    width: u32,
-    height: u32,
-}
-
-impl Rectangle {
-    fn area(&self) -> u32 {
-        self.width * self.height
-    }
-
-    fn can_hold(&self, other: &Rectangle) -> bool {
-        self.width > other.width && self.height > other.height
-    }
-
-    fn square(size: u32) -> Rectangle {
-        Rectangle {
-            width: size,
-            height: size,
-        }
-    }
 }
