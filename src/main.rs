ed10707861235fe257fb03e06c3be66a73298860
hk-pG <hukada.gogotea@gmail.com>
2026-02-22 21:32:09 +0900

    learn: usage of trait

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,59 @@
 fn main() {
-    let some_u8_value = Some(0u8);
+    let mut dolly: Sheep = Animal::new("Dolly");
 
-    if let Some(3) = some_u8_value {
-        println!("three");
-    } else {
-        ()
-    };
+    dolly.talk();
+    dolly.shear();
+    dolly.talk();
+}
+
+trait Animal {
+    fn new(name: &'static str) -> Self;
+    fn name(&self) -> &'static str;
+    fn noise(&self) -> &'static str;
+
+    fn talk(&self) {
+        println!("{} says {}", self.name(), self.noise());
+    }
+}
+
+struct Sheep {
+    naked: bool,
+    name: &'static str,
+}
+
+impl Sheep {
+    fn is_naked(&self) -> bool {
+        self.naked
+    }
+
+    fn shear(&mut self) {
+        if self.is_naked() {
+            println!("{} is already naked...", self.name());
+        } else {
+            println!("{} gets a haircut", self.name);
+            self.naked = true;
+        }
+    }
+}
+
+impl Animal for Sheep {
+    fn new(name: &'static str) -> Self {
+        Sheep { name, naked: false }
+    }
+
+    fn name(&self) -> &'static str {
+        self.name
+    }
+
+    fn noise(&self) -> &'static str {
+        if self.is_naked() {
+            "ツルッパゲやねん😡！"
+        } else {
+            "フッサフサやで😊！"
+        }
+    }
+
+    fn talk(&self) {
+        println!("{} pauses briefly... {}", self.name, self.noise());
+    }
 }
